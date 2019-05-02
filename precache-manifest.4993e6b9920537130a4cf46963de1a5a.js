1dfd2f3be7821185bd982c53d42dbb75d3478cce
viktorudachniy <viktorudachniy01@gmail.com>
2019-05-02 17:10:45 +0300

    first commit

---
+++ precache-manifest.4993e6b9920537130a4cf46963de1a5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "55e854213ab52b58d8d7",
+    "url": "/static/js/main.55e85421.chunk.js"
+  },
+  {
+    "revision": "c3d1eba2428becf3e1ce",
+    "url": "/static/js/2.c3d1eba2.chunk.js"
+  },
+  {
+    "revision": "55e854213ab52b58d8d7",
+    "url": "/static/css/main.64b637e0.chunk.css"
+  },
+  {
+    "revision": "c3d1eba2428becf3e1ce",
+    "url": "/static/css/2.30664705.chunk.css"
+  },
+  {
+    "revision": "b8135fbfc8b7a25e2341134aeddfe209",
+    "url": "/index.html"
+  }
+];
